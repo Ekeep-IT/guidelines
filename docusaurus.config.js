582b89c6cdbfaf5a2b5a7e707b6d3ce915521ec2
Gparquet <41692170+Gparquet@users.noreply.github.com>
2023-01-22 19:17:22 +0100

    feat(git-part): initialisation des guidlines de la partie git
    Co-authored-by: Geoffrey Parquet <geoffrey.parquet@viacesi.fr>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,15 +39,9 @@ const config = {
 			({
 				docs: {
 					sidebarPath: require.resolve('./sidebars.js'),
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
 				},
 				blog: {
 					showReadingTime: true,
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
-					editUrl: `https://github.com/${organizationName}/${projectName}/tree/main/`,
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
@@ -59,6 +53,9 @@ const config = {
 	themeConfig:
 		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
 		({
+			prims: {
+				additionalLanguages: ['java', 'csharp', 'cs', 'dotnet'],
+			},
 			navbar: {
 				title: 'My Site',
 				logo: {
