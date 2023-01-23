c6667bdede495ebb66e1f2b623f49e7b097d2453
Geoffrey Parquet <geoffrey.parquet@viacesi.fr>
2023-01-23 21:57:53 +0100

    refactor(footer): suppression des liens inutiles

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 const FeatureList = [
 	{
 		title: 'Guidelines Git',
 		img: 'git/logo.png',
 		alt: 'git-logo',
+    suffixeUrl: 'guidelines/docs/category/git',
 		description: <>Conventions, nommages, et bonnes pratiques git</>,
 	},
 	{
@@ -23,11 +25,13 @@ const FeatureList = [
 	},
 ];
 
-function Feature({ img, title, alt, description}) {
+function Feature({ img, baseUrl, suffixeUrl, title, alt, description}) {
   return (
 		<div className={clsx('col col--4')}>
 			<div className="text--center">
-				<img className={styles.featureSvg} alt={alt} src={require(`/img/${img}`).default} />
+				<a href={`${baseUrl}/${suffixeUrl}`}>
+					<img className={styles.featureSvg} alt={alt} src={require(`/img/${img}`).default} />
+				</a>
 			</div>
 			<div className="text--center padding-horiz--md">
 				<h3>{title}</h3>
@@ -38,12 +42,13 @@ function Feature({ img, title, alt, description}) {
 }
 
 export default function HomepageFeatures() {
+  const { siteConfig: {url} } = useDocusaurusContext();
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
+            <Feature key={idx} baseUrl={url} {...props} />
           ))}
         </div>
       </div>
